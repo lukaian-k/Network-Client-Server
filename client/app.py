827ca44b176fd6f031378de975a6347702b14264
Lucas José <lukinhas8979@gmail.com>
2023-10-09 17:45:00 -0300

    lukaian@09/10/23: alterando a estrutura da database e melhorando o codigo.

--- client/app.py
+++ client/app.py
@@ -1,17 +1,19 @@
 import tkinter as tk
 import socket
+from tkinter.simpledialog import askstring
 
-from tkinter.simpledialog import askstring 
+# Constants
+SERVER_ADDRESS = ("127.0.0.1", 8080)
 
 
-class NetworkClientApp:
+class ClientApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Network Client")
         self.root.geometry("500x650")
         self.root.configure(bg="#fbfaff")
         self.create_gui()
-        self.client = NetworkClient()
+        self.client = Client()
 
     def create_gui(self):
         self.create_header()
@@ -22,9 +24,9 @@ class NetworkClientApp:
         header_label = tk.Label(
             self.root,
             text="Comandos",
-            font=('Helvetica', 20, 'bold'),
+            font=("Helvetica", 20, "bold"),
             bg="#fbfaff",
-            fg="#606060"
+            fg="#606060",
         )
         header_label.pack(pady=10)
 
@@ -35,8 +37,8 @@ class NetworkClientApp:
         button_properties = {
             "bg": "#6d8d8c",
             "fg": "#fbfaff",
-            "font": ('Helvetica', 14, 'bold'),
-            "width": 20
+            "font": ("Helvetica", 14, "bold"),
+            "width": 20,
         }
 
         buttons = [
@@ -44,7 +46,7 @@ class NetworkClientApp:
             ("HORA", 1, False),
             ("ARQUIVO", 2, True),
             ("LISTAR", 3, False),
-            ("SAIR", 4, False)
+            ("SAIR", 4, False),
         ]
 
         for label, row, text_input in buttons:
@@ -52,7 +54,7 @@ class NetworkClientApp:
                 buttons_frame,
                 text=label,
                 command=self.send_command(label, text_input),
-                **button_properties
+                **button_properties,
             )
             button.grid(row=row, column=0, pady=5, sticky="w")
 
@@ -60,17 +62,13 @@ class NetworkClientApp:
         response_label = tk.Label(
             self.root,
             text="Resposta do servidor:",
-            font=('Helvetica', 14, 'bold'),
+            font=("Helvetica", 14, "bold"),
             bg="#fbfaff",
-            fg="#606060"
+            fg="#606060",
         )
         response_label.pack(pady=10)
         self.response_text = tk.Label(
-            self.root,
-            text="",
-            wraplength=300,
-            font=('Helvetica', 12),
-            bg="#fbfaff"
+            self.root, text="", wraplength=300, font=("Helvetica", 12), bg="#fbfaff"
         )
         self.response_text.pack()
 
@@ -78,32 +76,29 @@ class NetworkClientApp:
         def command_handler():
             if text_input:
                 user_input = askstring("Pop-up", f"{command}:")
-                
+
                 if user_input:
-                    response = self.client.send(f'{command}|{user_input}')
+                    response = self.client.send(f"{command}|{user_input}")
                 else:
                     return
-                
+
             else:
                 response = self.client.send(command)
 
-                if command == 'SAIR':
+                if command == "SAIR":
                     self.root.destroy()
-                
+
             self.update_response_text(response)
-            
+
         return command_handler
 
     def update_response_text(self, text):
-        self.response_text.config(
-            text=text,
-            fg="#6d8d8c"
-        )
+        self.response_text.config(text=text, fg="#6d8d8c")
 
 
-class NetworkClient:
+class Client:
     def __init__(self):
-        self.server_address = ('127.0.0.1', 8080)
+        self.server_address = SERVER_ADDRESS
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_socket.connect(self.server_address)
 
@@ -112,13 +107,12 @@ class NetworkClient:
             self.client_socket.send(command.encode())
             response = self.client_socket.recv(1024).decode()
             return response
-            
+
         except Exception as error:
             return f"Erro: {error}"
 
 
-
 if __name__ == "__main__":
     root = tk.Tk()
-    app = NetworkClientApp(root)
+    app = ClientApp(root)
     root.mainloop()
--- server/init.py
+++ server/init.py
@@ -1,10 +1,17 @@
-import socket, threading, os
+import socket
+import threading
+import os
 import json
-
 from datetime import datetime
 
 
-class NetworkServer:
+SERVER_HOST = ""
+SERVER_PORT = 8080
+DATABASE_DIR = "server/database"
+BDA_BIRTHDAYS_DIR = f"{DATABASE_DIR}/BDA_birthdays"
+
+
+class Server:
     def __init__(self, host, port):
         self.host = host
         self.port = port
@@ -13,23 +20,23 @@ class NetworkServer:
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.bind((self.host, self.port))
         server_socket.listen(5)
-        print(f"Servidor ouvindo na porta {self.port}")
+        print(f"Server listening on port {self.port}")
 
         try:
             while True:
                 client_socket, addr = server_socket.accept()
-                print(f"Conexão recebida de {addr}")
+                print(f"Connection received from {addr}")
 
-                client_handler = ClientHandler(client_socket)
+                client_handler = ClientHandlerThread(client_socket)
                 client_handler.start()
 
         except KeyboardInterrupt:
-            print("Servidor encerrado.")
+            print("Server terminated.")
         finally:
             server_socket.close()
 
 
-class ClientHandler(threading.Thread):
+class ClientHandlerThread(threading.Thread):
     def __init__(self, client_socket):
         super().__init__()
         self.client_socket = client_socket
@@ -46,7 +53,7 @@ class ClientHandler(threading.Thread):
                 self.client_socket.send(response.encode())
 
         except Exception as error:
-            print(f"Erro na conexão: {error}")
+            print(f"Connection error: {error}")
         finally:
             self.client_socket.close()
 
@@ -56,52 +63,48 @@ class ClientHandler(threading.Thread):
         if command == "HORA":
             time_now = datetime.now()
             formatted_time = time_now.strftime("%H:%M:%S")
-            return f'HORA_ATUAL: {formatted_time}'
+            return f"HORA_ATUAL: {formatted_time}"
 
         elif command == "LISTAR":
-            dir = "server/database"
-            files = os.listdir(dir)
-            return "LISTA_ARQUIVOS:\n\n"+"\n".join(files)
-        
+            files = os.listdir(BDA_BIRTHDAYS_DIR)
+            return "LISTA_ARQUIVOS:\n\n" + "\n".join(files)
+
         elif command == "SAIR":
-            print(f"ADEUS: {self.client_socket}")
+            print(f"Goodbye: {self.client_socket}")
             self.client_socket.close()
             return
-        
 
-        command, user_input = command.split('|')
+        command, user_input = command.split("|")
 
-        if command == 'CONSULTA':
-            with open('server/database/battle_rap_dictionary.json', 'r') as file:
+        if command == "CONSULTA":
+            with open(
+                os.path.join(DATABASE_DIR, "battle_rap_dictionary.json"), "r"
+            ) as file:
                 info_battle_rap = json.load(file)
-
                 info_battle_rap = {
-                    key.upper(): value
-                    for key, value in info_battle_rap.items()
+                    key.upper(): value for key, value in info_battle_rap.items()
                 }
 
                 if user_input in info_battle_rap:
-                    return f'CONSULTA:\n\n{user_input}: {info_battle_rap[user_input]}'
-                
-        elif command == 'ARQUIVO':
-            dir = "server/database"
-            files = os.listdir(dir)
+                    return f"CONSULTA:\n\n{user_input}: {info_battle_rap[user_input]}"
+
+        elif command == "ARQUIVO":
+            files = os.listdir(BDA_BIRTHDAYS_DIR)
 
             for file in files:
                 if user_input in file:
-                    with open(f'{dir}/{file}', 'r') as value:
+                    with open(os.path.join(BDA_BIRTHDAYS_DIR, file), "r") as value:
                         value = value.read()
-                        return f'{file}:\n\n{value}'
-                    
-            return 'ARQUIVO_NAO_ENCONTRADO'
-            
+                        return f"{file}:\n\n{value}"
+
+            return "ARQUIVO_NAO_ENCONTRADO"
+
         return "COMANDO_DESCONHECIDO"
 
 
 def main():
-    server = NetworkServer('', 8080)
+    server = Server(SERVER_HOST, SERVER_PORT)
     server.start()
-
 
 
 if __name__ == "__main__":
